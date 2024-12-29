0d6fdc152d37fee1f2196dbfdc1928ad688ce2f1
edulorenzo <edulorenzo@protonmail.com>
2024-12-29 19:08:59 +0100

    feature: add map view and user location in app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,52 @@
-import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import MapView, { Region } from "react-native-maps";
+
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null,
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [region, setRegion] = useState<Region | null>(null);
+
+  function convertToRegion(location: Location.LocationObject): Region {
+    return {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    };
+  }
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+    if (location) setRegion(convertToRegion(location));
+  }, [location]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location ? (
+        <MapView
+          style={styles.map}
+          region={region ?? undefined}
+          showsUserLocation={true}
+        />
+      ) : (
+        <Text>{errorMsg || "Loading..."}</Text>
+      )}
     </View>
   );
 }
@@ -16,5 +57,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
